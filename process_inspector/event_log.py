6ccecd923b051435341400b96795103d94d44769
Aravind Sankaran <a.sankaran@fz-juelich.de>
2025-08-15 14:01:22 +0200

    Added MetaData class

--- process_inspector/event_log.py
+++ process_inspector/event_log.py
@@ -21,4 +21,36 @@ class EventLog:
         pass
             
         
+  
+class MetaData:
+    def __init__(self):
+        self.case_attr = {}
+        self.obj_attr = {}
+        self.info = {}
+        
+    def add_case_attr(self, case, attr, value):
+        if case not in self.case_attr:
+            self.case_attr[case] = {}
+        self.case_attr[case][attr] = value
+        
+        
+    def add_obj_attr(self, obj, attr,value):
+        if obj not in self.obj_attr:
+            self.obj_attr[obj] = {}
+        self.obj_attr[obj][attr] = value
+        
+    def get_case_attr(self, case, attr):
+        value = None
+        try:
+           value = self.case_attr[case][attr]
+        except KeyError as e:
+            raise KeyError(f"Error: {e} for case {case} and attribute {attr}") 
+        return value
     
+    def get_obj_attr(self, obj, attr):
+        value = None
+        try:
+            value = self.obj_attr[obj][attr]
+        except KeyError as e:
+            raise KeyError(f"Error: {e} for object {obj} and attribute {attr}") 
+        return value
